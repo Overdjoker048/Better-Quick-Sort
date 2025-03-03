1e8ffc565646e501c718edee66d6b52df5c6da74
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2025-03-03 09:21:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 def sort(tab : list) -> list[int]:
     dico: dict[int, int] = {}
-    maxv: int = tab[0]
-    minv: int = tab[0]
-    step = 1
     for i in tab:
-        if i > maxv:
-            maxv = i
-        elif i < minv:
-            minv = i
-        istep = 10 ** -(len(str(i).split('.')[1]))
-        if istep < step:
-            step = istep
         if dico.get(i):
             dico[i] += 1
         else:
             dico[i] = 1
     tab = []
-    for i in range(minv, maxv+1, step):
+    for i in range(min(tab), max(tab)+1, step):
         for _ in range(dico.get(i, 0)):
             tab.append(i)
     return tab
