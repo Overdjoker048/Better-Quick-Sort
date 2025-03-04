165fd60f7a4d18ee59528d0a6c6af873bad77239
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2025-03-04 10:52:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 def sort(tab : list) -> list[int]:
     dico: dict[int, int] = {}
     for i in tab:
-        if dico.get(i):
-            dico[i] += 1
-        else:
-            dico[i] = 1
+        dico[i] = dico.get(i, 0) + 1
     tab = []
     for i in range(min(tab), max(tab)+1, step):
         for _ in range(dico.get(i, 0)):
