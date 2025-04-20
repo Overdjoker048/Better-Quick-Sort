6e804af005c944e2493795541289c0913cf7b3c9
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2025-04-20 23:32:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def sort(tab: list) -> list[int]:
+def better_quick_sort(tab: list) -> list[int]:
+    #Return an empty list if tab is empty
+    if not tab:
+        return []
+
     # Create a dictionary to count occurrences of each number
-    dico: dict[int, int] = {}
+    count: dict[int, int] = {}
+    
     for i in tab:
-        dico[i] = dico.get(i, 0) + 1
-    
+        count[i] = count.get(i, 0) + 1
+
     # Initialize an empty list to store the sorted numbers
-    tab = []
+    result = []
     
-    # Iterate over the range from the minimum to the maximum value in the input list
+    #Iterate over the range from the minimum to the maximum value in the input list
     for i in range(min(tab), max(tab) + 1):
-        # Append each number to the sorted list according to its frequency
-        for _ in range(dico.get(i, 0)):
-            tab.append(i)
-    
-    # Return the sorted list
-    return tab
+        #Append each number to the sorted list according to its frequency
+        c = count.get(i, 0)
+        if c != 0:
+            result.extend([i] * c)
+
+    #Return the sorted list
+    return result
