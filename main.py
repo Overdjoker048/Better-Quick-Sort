25233e283fa2c8bddab00d7f3c5986c74b85646b
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2025-05-06 15:50:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+import math
+
 def better_quick_sort(tab: list) -> list[int]:
-    #Return an empty list if tab is empty
     if not tab:
         return []
-
-    # Create a dictionary to count occurrences of each number
-    count: dict[int, int] = {}
     
+    deci = None
+    count: dict[float, int] = {}
+
     for i in tab:
         count[i] = count.get(i, 0) + 1
-
-    # Initialize an empty list to store the sorted numbers
+        decil = math.floor(math.log10(abs(i))) - 2
+        if deci is None or decil < deci:
+            deci = decil
+    multi = 10 ** (-1*deci)
     result = []
-    
-    #Iterate over the range from the minimum to the maximum value in the input list
-    for i in range(min(tab), max(tab) + 1):
-        #Append each number to the sorted list according to its frequency
+    for i in range(math.floor(min(tab))*multi, math.ceil(max(tab))*multi + 1):
+        i = i/multi
         c = count.get(i, 0)
         if c != 0:
             result.extend([i] * c)
-
-    #Return the sorted list
     return result
